b19f32681b4e12e7894cd355066d94ca5ed457c2
RobertYuan <634954435@qq.com>
2022-10-30 18:14:16 +0800

    10.30 from laptap

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,8 +6,6 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
@@ -18,7 +16,7 @@ struct JobStatus {
 
 fn main() {
     let status = Arc::new(JobStatus { jobs_completed: 0 });
-    let status_shared = status.clone();
+    let status_shared = Arc::clone(&status);
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
